6d06d6f25d6459ed26e745f0fd7c2e2de95af55b
dfb365005 <dfb365005@long-chen>
2014-10-28 21:46:44 +0800

    maven project init

--- WxGroup/src/com/wx/group/module/demo/model/Classes.java
+++
@@ -1,9 +0,0 @@
-package com.wx.group.module.demo.model;
-
-import com.jfinal.plugin.activerecord.Model;
-
-public class Classes extends Model<Classes> {
-	private static final long serialVersionUID = 1L;
-	public static final Classes dao = new Classes();
-	
-}
